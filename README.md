da7ebb2d8288ff3fdc2916158b89729cea34c5e2
Jim McAndrew <jim@loc8.us>
2018-09-06 12:43:15 -0600

    Take params

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # esri-query
 Extracts data from ESRI REST endpoints when nothing else will
+
+usage:
+node index.js URL FILENAME
+
+example:
+node index.js "http://maps.pasda.psu.edu/arcgis/rest/services/pasda/CityPhillyWatersheds/MapServer/8'" philly_watersheds.geojson
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var esriQuery = require('./src/esri-query');
 var fs = require('fs');
 var crypto = require('crypto');
 
-var url = 'https://mapservices.nps.gov/arcgis/rest/services/NPS_Public_Trails_Geographic/FeatureServer/0';
+var url = process.argv[2];
+// var url = 'https://mapservices.nps.gov/arcgis/rest/services/NPS_Public_POIs/FeatureServer/0';
 // var url = 'http://maps.pasda.psu.edu/arcgis/rest/services/pasda/CityPhillyWatersheds/MapServer/8';
 var whereObj = {
   'where': '1=1',
@@ -12,7 +13,8 @@ var whereObj = {
 };
 var returnFields = null; // [];
 var sourceInfo = null;
-var outFile = './trails2.json';
+var outFile= process.argv[3];
+// var outFile = './pois.json';
 // var outFile = './test.json';
 var options = {
   'asGeoJSON': true

3b51537e7caf5f305c7edcca1ee8d7cd9ede003e
Jim McAndrew <jim@loc8.us>
2018-09-24 17:20:52 -0600

    Add writer package

---
+++ src/writer.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+var Writer = function(filename, type, options) {
+  // Types
+  var types = {
+    'file': true,
+    'geopackage': true,
+  };
+
+  return {
+    'create': function(options){},
+    'append': function(options){},
+    'save': function(options){},
+    'close': function(options){}
+  }'
+};

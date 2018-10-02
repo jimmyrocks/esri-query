e75747f9390fc544b74d3c9cf4bba0f662c594a2
Jim McAndrew <jim@loc8.us>
2018-10-02 10:21:48 -0600

    Fix tests

--- test/app-test.js
+++ test/app-test.js
@@ -2,23 +2,22 @@
 
 var App = require('../');
 
-var test = function() {
-  var reader = new App();
-  var stream = reader.stream;
+var test = function () {
+  var writer = new App();
+  var stream = writer.stream;
 
   stream.on('data', (chunk) => {
     process.stdout.write(chunk);
   });
-  stream.on('end', (chunk) => {
+  stream.on('end', () => {
     console.error('done');
   });
 
-  reader.query('http://maps.pasda.psu.edu/arcgis/rest/services/pasda/CityPhillyWatersheds/MapServer/3', 'OBJECTID<=50').then(function() {
-    console.log('----');
-  }).catch(function(e) {
-    console.log('Error', e);
+  writer.query('http://maps.pasda.psu.edu/arcgis/rest/services/pasda/CityPhillyWatersheds/MapServer/3', 'OBJECTID<=50').then(function () {
+    console.error('----');
+  }).catch(function (e) {
+    console.error('Error', e);
   });
-
 };
 
 test();

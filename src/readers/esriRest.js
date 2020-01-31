43181e4c427a7548edbfd47da8c21ef49d54ee94
Jim McAndrew <jim@loc8.us>
2020-01-31 13:23:48 -0700

    Add order by support: Update src/readers/esriRest.js

--- src/readers/esriRest.js
+++ src/readers/esriRest.js
@@ -9,26 +9,25 @@ const crypto = require('crypto');
 const splitBbox = require('../helpers/esri-split-bbox');
 const post = require('../helpers/post-async');
 
-var startQuery = function (sourceUrl, origQueryObj, primaryKeys, sourceInfo, writer, options) {
+var startQuery = function(sourceUrl, origQueryObj, primaryKeys, sourceInfo, writer, options) {
   if (sourceInfo) {
     return runQuery(sourceUrl, origQueryObj, primaryKeys, sourceInfo, options, writer);
   } else {
     return post(sourceUrl, {
       'f': 'json'
-    }).then(function (source) {
-      var fields = source.fields.map(function (field) {
-        return field.name;
-      });
+    }).then(function(source) {
+      var fields = source.fields.filter(field => field.type !== 'esriFieldTypeGeometry').map(field => field.name);
+      options['feature-count'] = options['feature-count'] || source.maxRecordCount;
       return runQuery(sourceUrl, origQueryObj, primaryKeys || fields, source, options, writer);
-    }).catch(function (e) {
-      return new Promise(function (f, r) {
+    }).catch(function(e) {
+      return new Promise(function(f, r) {
         r(e);
       });
     });
   }
 };
 
-var expandBbox = function (newBox, origBox) {
+var expandBbox = function(newBox, origBox) {
   origBox = origBox || [Infinity, Infinity, -Infinity, -Infinity];
   origBox[0] = origBox[0] > newBox[0] ? newBox[0] : origBox[0];
   origBox[1] = origBox[1] > newBox[1] ? newBox[1] : origBox[1];
@@ -37,21 +36,20 @@ var expandBbox = function (newBox, origBox) {
   return origBox;
 };
 
-var runQuery = function (sourceUrl, origQueryObj, primaryKeys, sourceInfo, options, writer) {
+var runQuery = function(sourceUrl, origQueryObj, primaryKeys, sourceInfo, options, writer) {
   // Returns the ESRI Output JSON
   var taskList = [];
   var bbox;
   var hashList = {};
-  var first = true;
   var errorCount = 0;
 
-  var reduceQuery = function (sourceUrl, queryObj, primaryKeys, extent) {
+  var reduceQuery = function(sourceUrl, queryObj, primaryKeys, extent) {
     var newExtents = splitBbox(extent);
     // console.error('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-');
     // console.error('Splitting: ', extent);
     // console.error('To: ', newExtents);
     // console.error('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-');
-    newExtents.forEach(function (newExtent) {
+    newExtents.forEach(function(newExtent) {
       taskList.push({
         'name': 'Query ' + JSON.stringify(newExtent),
         'description': 'Partial Extent Query',
@@ -61,27 +59,132 @@ var runQuery = function (sourceUrl, origQueryObj, primaryKeys, sourceInfo, optio
     });
   };
 
-  var queryServer = function (sourceUrl, queryObj, primaryKeys, extent) {
+  var nextQuery = function(sourceUrl, queryObj, primaryKeys, split) {
+    var newQuerys = [];
+    if (!split) {
+      newQuerys.push({
+        'min': queryObj.resultOffset + options['feature-count'],
+        'max': queryObj.resultOffset + options['feature-count'] + options['feature-count']
+      });
+    } else {
+      // Cut the request in half
+      newQuerys.push({
+        'min': queryObj.resultOffset + options['feature-count'],
+        'max': queryObj.resultOffset + options['feature-count'] + Math.floor(options['feature-count'] / 2)
+      });
+      newQuerys.push({
+        'min': queryObj.resultOffset + options['feature-count'] + Math.floor(options['feature-count'] / 2),
+        'max': queryObj.resultOffset + options['feature-count'] + options['feature-count']
+      });
+
+    }
+    newQuerys.forEach(function(range) {
+      taskList.push({
+        'name': 'Query ' + JSON.stringify(range),
+        'description': 'Partial Extent Query',
+        'task': queryServer,
+        'params': [sourceUrl, queryObj, primaryKeys, range]
+      });
+    });
+  };
+
+
+  var queryServer = function(sourceUrl, queryObj, primaryKeys, extent) {
     var newQueryObj = JSON.parse(JSON.stringify(queryObj));
+    // Make it a query URL if it isn' already one
+    sourceUrl = sourceUrl.replace(/query\??$/ig, '');
+    sourceUrl = sourceUrl + (sourceUrl.substr(sourceUrl.length - 1) === '/' ? '' : '/') + 'query';
+    newQueryObj.f = 'json';
+
+    // Get URL
+    console.error('getting url', sourceUrl, newQueryObj, options);
+    return options.method === 'geographic' ?
+      runGeometryQuery(sourceUrl, newQueryObj, extent, primaryKeys) :
+      runPaginatedQuery(sourceUrl, newQueryObj, extent, primaryKeys);
+  };
+
+  var runPaginatedQuery = function(sourceUrl, newQueryObj, extent, primaryKeys) {
+    extent.min = extent.min === undefined ? 0 : extent.min;
+    extent.max = extent.max === undefined ? options['feature-count'] : extent.max;
+
+    newQueryObj.outFields = primaryKeys.join(',');
+    newQueryObj.orderByFields = newQueryObj.outFields;
+    newQueryObj.returnGeometry = true;
+    newQueryObj.resultRecordCount = (extent.max - extent.min);
+    newQueryObj.resultOffset = extent.min;
+    // console.error('Getting url', sourceUrl, newQueryObj);
+
+    return post(sourceUrl, newQueryObj).then(function(data) {
+      return new Promise(function(resolve) {
+        // console.error('Got url!');
+        //
+        if (data.features && data.features.length === 0) {
+          console.error('NO FEATURES LEFT');
+          // No features in this query
+          resolve(null);
+        } else if (data.features) {
+          // console.error('features!', data.features.length);
+          // Get next
+          nextQuery(sourceUrl, newQueryObj, primaryKeys, true);
+          errorCount--;
+          writeOut(data); // TODO: transform it?
+          resolve(null);
+        } else if (data.error) {
+          console.error('* ERROR with query  **********************************');
+          console.error(data.error);
+          console.error('* ERROR ************************************');
+          errorCount--;
+          nextQuery(sourceUrl, newQueryObj, primaryKeys, true);
+          resolve(null);
+        } else {
+          // Not much else we can do? error? null?
+          console.error('???????????????????????????');
+          process.exit();
+          resolve(null);
+        }
+      });
+    }).catch(e => {
+      return new Promise(function(resolve, reject) {
+        // TODO, actually fail on 404s
+        console.error('Error with request');
+        console.error(e.status, e.code);
+        if (e.code === 'ECONNRESET') {
+          nextQuery(sourceUrl, newQueryObj, primaryKeys, true);
+          resolve(null);
+        } else if (e.status === 502 || e.status === 504) {
+          // If we're getting a 502, wait
+          errorCount = errorCount <= 0 ? 0 : errorCount;
+          errorCount++;
+          if (errorCount > 10) {
+            console.error('Too many errors');
+            console.error(e.status, e.code);
+            reject(e);
+          }
+          setTimeout(function() {
+            nextQuery(sourceUrl, newQueryObj, primaryKeys, true);
+            resolve(null);
+          }, 1500 * errorCount);
+        } else {
+          console.error('ending');
+          reject(e);
+        }
+      });
+    });
+  };
+
+  var runGeometryQuery = function(sourceUrl, newQueryObj, extent, primaryKeys) {
     // Add bounding box query
     newQueryObj.inSR = (extent && extent.spatialReference && extent.spatialReference.wkid) || newQueryObj.inSR || null;
     newQueryObj.geometry = extent ? [extent.xmin, extent.ymin, extent.xmax, extent.ymax].join(',') : null;
     newQueryObj.geometryType = 'esriGeometryEnvelope';
     newQueryObj.spatialRel = 'esriSpatialRelIntersects';
-    newQueryObj.f = 'json';
 
     // Check to see if we're going to go over the max limit
     newQueryObj.outFields = primaryKeys ? primaryKeys[0] : '';
     newQueryObj.returnGeometry = false;
 
-    // Make it a query URL if it isn' already one
-    sourceUrl = sourceUrl.replace(/query\??$/ig, '');
-    sourceUrl = sourceUrl + (sourceUrl.substr(sourceUrl.length - 1) === '/' ? '' : '/') + 'query';
-
-    // Get URL
-    console.error('getting url', sourceUrl, newQueryObj);
-    return post(sourceUrl, newQueryObj).then(function (data) {
-      return new Promise(function (resolve) {
+    return post(sourceUrl, newQueryObj).then(function(data) {
+      return new Promise(function(resolve) {
         console.error('prequery finished');
         if (data.exceededTransferLimit) {
           console.error('EXCEEDED', extent);
@@ -101,7 +204,7 @@ var runQuery = function (sourceUrl, origQueryObj, primaryKeys, sourceInfo, optio
           //   reduceQuery(sourceUrl, newQueryObj, primaryKeys, extent);
           //   resolve(null);
           // }
-          return post(sourceUrl, newQueryObj).then(function (data) {
+          return post(sourceUrl, newQueryObj).then(function(data) {
             // We got the data!
             if (data && data.error) {
               console.error('* ERROR getting features **********************************');
@@ -114,7 +217,7 @@ var runQuery = function (sourceUrl, origQueryObj, primaryKeys, sourceInfo, optio
               writeOut(data); // TODO: transform it?
               resolve(null);
             }
-          }).catch(function () {
+          }).catch(function() {
             // There was some kind of error, check it, and maybe split bbox?
             reduceQuery(sourceUrl, newQueryObj, primaryKeys, extent);
             resolve(null);
@@ -135,15 +238,15 @@ var runQuery = function (sourceUrl, origQueryObj, primaryKeys, sourceInfo, optio
           resolve(null);
         }
       });
-    }).catch(function (e) {
-      return new Promise(function (resolve, reject) {
+    }).catch(function(e) {
+      return new Promise(function(resolve, reject) {
         // TODO, actually fail on 404s
         console.error('Error with request');
         console.error(e.status, e.code);
         if (e.code === 'ECONNRESET') {
           reduceQuery(sourceUrl, newQueryObj, primaryKeys, extent);
           resolve(null);
-        } else if (e.status = 502) {
+        } else if (e.status === 502) {
           // If we're getting a 502, wait
           errorCount = errorCount <= 0 ? 0 : errorCount;
           errorCount++;
@@ -163,12 +266,12 @@ var runQuery = function (sourceUrl, origQueryObj, primaryKeys, sourceInfo, optio
     });
   };
 
-  var writeOut = function (result) {
+  var writeOut = function(result) {
     var esriOptions = {
       'sr': (result && result.spatialReference && (result.spatialReference.latestWkid || result.spatialReference.wkid)) || null
     };
     if (result && result.features) {
-      result.features.forEach(function (feature) {
+      result.features.forEach(function(feature) {
         feature = feature || {};
         var geometry = null;
         try {
@@ -207,13 +310,13 @@ var runQuery = function (sourceUrl, origQueryObj, primaryKeys, sourceInfo, optio
     'params': [sourceUrl, origQueryObj, primaryKeys, sourceInfo.extent]
   });
 
-  return runList(taskList).then(function () {
+  return runList(taskList).then(function() {
     writer.writeFooter(bbox);
   });
 };
 
 
-module.exports = function (url, whereObj, primaryKeys, sourceInfo, options, writer) {
+module.exports = function(url, whereObj, primaryKeys, sourceInfo, options, writer) {
   /* Valid Options
    * 'sr': output SR
    */
